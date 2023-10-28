3438e6f0ededd72a901e44a56bd29c0660856f34
emba2ra3star <122137591+emba2ra3star@users.noreply.github.com>
2023-10-28 22:59:54 +0800

    Add files via upload

---
+++ script/js_index.js
@@ -0,0 +1,8 @@
+function toggleNavigation() {
+    let navigation = document.getElementById("nav");
+    if (navigation.style.display === "none") {
+        navigation.style.display = "block";
+    } else {
+        navigation.style.display = "none";
+    }
+}
